e60f6c4ec167d9e4ff9decbad8e0f609496907ef
Ethan Stucker <ethan.stucker@proton.me>
2025-10-08 21:20:07 -0500

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = false -- Disable relative numbers
+
+vim.opt.tabstop = 4 -- Number of spaces a <Tab> counts for
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.expandtab = false -- Use spaces instead of tabs
+vim.opt.softtabstop = 4 -- Number of spaces to insert for a <Tab>
+vim.g.autoformat = false
+vim.cmd([[set noexpandtab]])
---
+++ lua/plugins/themes.lua
@@ -0,0 +1,19 @@
+return {
+  -- First, add the Everforest theme plugin itself
+  {
+    "sainnhe/everforest",
+    priority = 1000, -- Make sure it loads first
+    config = function()
+      vim.g.everforest_background = "medium"
+      vim.cmd.colorscheme("everforest")
+    end,
+  },
+
+  -- Second, tell LazyVim to use "everforest" as the default colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
